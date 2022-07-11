8bd62658c91f7ffe2503a884844c6f8c10e48641
cedoq <cedoq2@gmail.com>
2022-07-11 22:33:03 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
         System.out.println("");
 
         System.out.println("компания - " + Arrays.toString(company));
-        System.out.println("игрок - " + Arrays.toString(company));
+        System.out.println("игрок - " + Arrays.toString(player));
         System.out.println("количество совпадений: " + k);
     }
 }
