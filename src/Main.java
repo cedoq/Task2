97b6048390e14b2a98a42ae13d3d77e1e40b4bc5
cedoq <cedoq2@gmail.com>
2022-07-11 17:52:41 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        int k = 0; //count
+        int[] company = new int[7];
+        int[] player = new int[7];
+        System.out.print("совпали номера элементов -> ");
+        for(i = 0; i<= company.length-1;i++){
+            company[i] = (int)( Math.random()*10);
+            player[i] = (int)( Math.random()*10);
+            if(player[i] == company[i]){
+                System.out.print(i + " ");
+                k++;
+
+            }
+
+        }
+        System.out.println("");
+
+        System.out.println("компания - " + Arrays.toString(company));
+        System.out.println("игрок - " + Arrays.toString(company));
+        System.out.println("количество совпадений: " + k);
+    }
+}
